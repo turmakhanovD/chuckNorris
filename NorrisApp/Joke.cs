b7208ac21fca2e196a5eeaf6f236c969abaa183a
Турмахан Динислам Даулетулы <@corp.itstep>
2019-06-07 17:49:19 +0600

    first commit

---
+++ NorrisApp/Joke.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace NorrisApp
+{
+    public class Joke
+    {
+
+        [JsonProperty("icon_url")]
+        public string IconUrl { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
---
+++ NorrisApp/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NorrisApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        const string url = @"https://api.chucknorris.io/jokes/random";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetJoke(object sender, RoutedEventArgs e)
+        {
+            if (comboBox.SelectedIndex == -1)
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.ContentType = "text/json";
+                httpWebRequest.Method = "GET";
+
+                Task.Run(() =>
+                {
+                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+
+                        Joke joke = JsonConvert.DeserializeObject<Joke>(result);
+
+                        MessageBox.Show(joke.Value, "Смешная шутка!");
+                    }
+                });
+            }
+            else
+            {
+                ComboBoxItem typeItem = (ComboBoxItem)comboBox.SelectedItem;
+                string value = typeItem.Content.ToString();
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url + $"?category={value}");
+                httpWebRequest.ContentType = "text/json";
+                httpWebRequest.Method = "GET";
+
+                Task.Run(() =>
+                {
+                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+
+                        Joke joke = JsonConvert.DeserializeObject<Joke>(result);
+
+                        MessageBox.Show(joke.Value, "Смешная шутка!");
+                    }
+                });
+            }
+        }
+    }
+}
